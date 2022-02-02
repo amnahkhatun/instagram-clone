58a967806283f4ed6bcd5349311bed70c7d2f7e5
amnahkhatun <amna.khatun1605@gmail.com>
2022-02-02 12:21:47 +0530

    day 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+import instagram from './images/Instagram-Logo.png';
+import avatar from './images/logo.jpeg';
+import Posts from './components/Posts';
+
 function App() {
   return (
     <div className="App">
-      Hello world
+      <header className="app__header" >
+        <img className="app__headerImage" alt="logo" src={instagram} width="120px"></img>
+        <img className="app__headerAvatar" alt="logo" src={avatar} width="100px"></img>
+      </header>
+      <Posts />
     </div>
   );
 }
