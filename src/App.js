0d068d27ea8cd8b81fc73e95cf3767ee5c03c8d2
amnahkhatun <amna.khatun1605@gmail.com>
2022-02-04 12:01:53 +0530

    day 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import React, { useState } from 'react'
+
+
 import './App.css';
 
 import instagram from './images/Instagram-Logo.png';
 import avatar from './images/logo.jpeg';
 import Posts from './components/Posts';
+import miles from './images/miles.jpeg';
+
+import { db } from './firebase.js'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      imageUrl: miles,
+      caption: "Hello",
+      username: "amnah"
+    }
+  ])
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot)
+  }, [input])
+
   return (
     <div className="App">
       <header className="app__header" >
         <img className="app__headerImage" alt="logo" src={instagram} width="120px"></img>
         <img className="app__headerAvatar" alt="logo" src={avatar} width="100px"></img>
       </header>
-      <Posts />
+      {posts.map(({ imageUrl, username, caption }) => (
+        <Posts imageUrl={imageUrl} caption={caption} username={username} />
+      ))}
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBERTquT94UnDVOCWcOxBRbm9idxBCPJAI",
+  authDomain: "instagram-clone-f69ba.firebaseapp.com",
+  projectId: "instagram-clone-f69ba",
+  storageBucket: "instagram-clone-f69ba.appspot.com",
+  messagingSenderId: "485350832717",
+  appId: "1:485350832717:web:7aa3a800b9f9a0aa04fa04",
+  measurementId: "G-HM5LXSBDT6"
+})
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage }
