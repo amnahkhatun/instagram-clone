c6d1b919699d6133801298c9ae2dc5d97e3cc174
amnahkhatun <amna.khatun1605@gmail.com>
2022-02-25 16:48:59 +0530

    logout/login and progress bar functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import avatar from './images/logo.jpeg';
 import Posts from './components/Posts';
 import miles from './images/miles.jpeg';
 
-import { db } from './firebase'
+import { db, auth } from './firebase'
 import { Modal } from '@material-ui/core';
 
 import Box from '@mui/material/Box';
@@ -16,6 +16,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Input } from '@mui/material';
 import { sendSignInLinkToEmail } from '@firebase/auth';
+import ImageUpload from './components/ImageUpload';
 
 
 const modalStyle = {
@@ -36,8 +37,27 @@ function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [username, setUsername] = useState('')
+  const [user, setUser] = useState(null)
+  const [openSignIn, setOpenSignIn] = useState(false)
 
 
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log({authUser});
+        setUser(authUser)
+      }
+      else {
+        setUser(null)
+      }
+
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [user, username])
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -47,52 +67,97 @@ function App() {
     })
   }, [])
 
-  const signup = () => {
-    console.log("hi")
+  const signup = (event) => {
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username
+        })
+      })
+      .catch((err) => (err.message))
+  }
+  const signIn = (event) => {
+    event.preventDefault()
+    auth.signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message))
+
+    setOpenSignIn(false)
   }
 
   return (
     <div className="App">
+      {true ? <ImageUpload username="amnah" /> :
+        <h3>You need to login again</h3>}
+{console.log({user})}
+
       <Modal
         open={open} onClose={() => setOpen(false)}>
         <Box sx={modalStyle}>
-          <form>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Text in a modal
-          </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              <Input
-                placeholder="username"
-                type="text"
-                value={username}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Input
-                placeholder="email"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Input
-                placeholder="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button onClick={signup}>Sign Up</Button>
-            </Typography>
+          <form className="app__signup">
+            <img className="app__headerImage" alt="logo" src={instagram} width="120px"></img>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signup}>Sign Up</Button>
           </form>
         </Box>
       </Modal>
+      <Modal
+        open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <Box sx={modalStyle}>
+          <form className="app__signup">
+            <img className="app__headerImage" alt="logo" src={instagram} width="120px"></img>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>Sign Up</Button>
+          </form>
+        </Box>
+      </Modal>
+
       <header className="app__header" >
         <img className="app__headerImage" alt="logo" src={instagram} width="120px"></img>
         <img className="app__headerAvatar" alt="logo" src={avatar} width="100px"></img>
       </header>
-      <Button onClick={() => setOpen(true)}>Sign up</Button>
+      {user ?
+        <Button onClick={() => auth.signOut()}>Log out</Button> :
+
+        (<div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign up</Button>
+        </div>)
+
+
+      }
+
       {posts.map(({ imageUrl, username, caption }) => (
         <Posts imageUrl={imageUrl} caption={caption} username={username} />
       ))}
-
     </div>
   );
 }
